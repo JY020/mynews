f3ef282973386313d2d0ed2c6659f0d0dd9c3104
JY020 <3155021@kuis.ac.jp>
2019-11-25 09:52:26 +0000

    課題laravel12

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -11,4 +11,9 @@ public function add()
   {
       return view('admin.news.create');
   }
+  
+  public function create(Request $request)
+  {
+    return redirect ('admin/news/create');
+  }
 }
--- routes/web.php
+++ routes/web.php
@@ -18,11 +18,30 @@ Route::get('/', function () {
 
 
 Route::group(['prefix' => 'admin'], function(){
-  Route::get('news/create', 'Admin\NewsController@add');
+  Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
   Route::get('XXX', 'AAAController@bbb');
-  Route::get('profile/create', 'Admin\ProfileController@add');
-  Route::get('profile/edit', 'Admin\ProfileController@edit');
+  Route::get('profile/create', 'Admin\ProfileController@add')->middleware('auth');
+  Route::get('profile/edit', 'Admin\ProfileController@edit')->middleware('auth');
+});
+
+Route::group(['prefix' => 'admin','middleware' => 'auth'], function() {
+  Route::get('news/create', 'Admin\NewsController@add');
+  Route::post('news/create', 'Admin\NewsController@create');
+});
+
+Route::group(['prefix' => 'admin'], function() {
+   Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
 });
 
 
+Auth::routes();
 
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
